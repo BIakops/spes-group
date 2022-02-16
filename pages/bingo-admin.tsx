81447e1ed061669773640ac948976735d6d73049
Your Name <you@example.com>
2022-02-16 11:54:50 +0800

    First Creation Branch

---
+++ pages/bingo-admin.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+export default class bingo extends React.Component{
+    /// Let this be the bingo wheel
+    /// Create Const arr for possible numbers in Bingo
+    
+    
+
+
+
+    state ={
+
+    }
+    render(): React.ReactNode {
+        return(
+            <div className = "px-4 bg-blue-700">This will be my admin view
+            <p>some code written</p>
+            </div>
+            
+        )
+    }
+}
